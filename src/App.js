69568142f4a11cafacda914e183775c3872682f1
Gabriel Favilla Gugelmin <gaab.fg@gmail.com>
2021-06-10 21:28:50 -0300

    added inputs to sum numbers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { sum } from './calculator'
 import './App.css';
 
 function App() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+
+  const submitHandler = e => {
+    e.preventDefault();
+    const result = sum(number1, number2);
+    console.log('result: ', result)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitHandler}>
+        <div>
+          <input type="number" name="number1" placeholder="Insira o primeiro número" value={number1} onChange={(e) => setNumber1(e.target.value)} />
+        </div>
+
+        <div>
+          <input type="number" name="number2" placeholder="Insira o segundo número" value={number2} onChange={(e) => setNumber2(e.target.value)} />
+        </div>
+
+        <div>
+          <button>somar</button>
+        </div>
+      </form>
     </div>
   );
 }
